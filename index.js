abeb1838de5fa0a64b26aa720e0e2961ab106617
Martin Fogliacco <fogliaccomartin3@gmail.com>
2022-09-24 20:44:33 -0300

    prueba

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+
+    if(numero%2 === 0){
+        return (numero >>> 0).toString(2)
+    } else{
+        return (numero >>> 0).toString(16)
+    }
 }
+
+
+function martin(){
+    return {edad: 20, nacionalidad: "Argentina"}
+}
+
+
+console.log(laCajaDePandora())
