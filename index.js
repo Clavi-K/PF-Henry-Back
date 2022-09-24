b452c8cba0d3a0528bb1457bccb43151baf5f7e3
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-09-24 20:44:10 -0300

    belen

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+
+    if(numero % 2 === 0) return (numero >>> 0).toString(2)
+
+    return (numero >>> 0).toString(16)
+
 }
