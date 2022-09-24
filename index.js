3c1e76a4c8e53347ee359a5f2f6034f4f16d4d25
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-09-24 20:41:48 -0300

    (feat) function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+
+    if(numero % 2 == 0) {
+        return (numero >>> 0).toString(2)
+    } else {
+        return numero.toString(16)
+    }
+
 }
