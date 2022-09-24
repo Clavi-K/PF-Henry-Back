741858cb62231f0cb9a9c025a7ce95555b3750f9
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-09-24 20:44:45 -0300

    otro de sergio

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+    if(numero%0){
+        return(numero >>> 0).toString(2)
+    } else {
+        return(numero >>> 0).toString(16)
+    }
+}
+
+function sergio() {
+    return{
+        edad:31,
+        nacionalidad:'Argentina'
+    }
 }
