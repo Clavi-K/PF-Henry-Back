5e6f7a4f13bb2b6729dd38bff42b76770d88ffee
Angela Testino <angela11021997@gmail.com>
2022-09-24 18:54:29 -0500

    angela first commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+    if(numero%2===0){
+        return (numero).toString(2)
+    }
+    return (numero).toString(16)
 }
