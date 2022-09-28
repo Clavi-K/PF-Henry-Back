274d255d238ae00359350545efffe554977475c1
Angela Testino <angela11021997@gmail.com>
2022-09-27 19:58:00 -0500

    cors añadido

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config')
 const routes = require('./routers/index');
+const cors = require('cors')
 
 
 
@@ -35,6 +36,19 @@ mongoose.connect(`${config.atlas.SCHEMA}://${config.atlas.USER}:${config.atlas.P
 
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+
+    //cors añadido 
+    app.use(cors())
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header("Access-Control-Allow-Credentials", 'true');
+      res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+})//------
 
     /* ========== */
 
