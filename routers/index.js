6154c2e1379b85f9e9ad60be0532362276b5dd3e
Angela Testino <angela11021997@gmail.com>
2022-10-06 16:15:13 -0500

    pasarela mp

--- routers/index.js
+++ routers/index.js
@@ -1,39 +1,42 @@
 const express = require("express");
-const logger = require("../utils/logger")
+const logger = require("../utils/logger");
 
 //Se importan todas las rutas
-const ApiRouter = require("./api.router.js")
-const showtimeRouter = require("./showtime.router.js")
-const reviewRouter = require("./review.router.js")
-const roomRouter = require("./room.router")
-const reservationRouter = require("./reservation.router")
-const authRouter = require("./auth.router")
-
+const ApiRouter = require("./api.router.js");
+const showtimeRouter = require("./showtime.router.js");
+const reviewRouter = require("./review.router.js");
+const roomRouter = require("./room.router");
+const reservationRouter = require("./reservation.router");
+const authRouter = require("./auth.router");
+const paymentRouter = require("./payment.router");
 const router = express.Router();
 
-//Se definen las rutas base 
-router.use("/auth", authRouter)
-router.use('/api', ApiRouter)// ejemplo:http://localhost:8082/api/popular
-router.use('/showtime', showtimeRouter)
-router.use('/review', reviewRouter)
-router.use('/room', roomRouter)
-router.use('/reservation', reservationRouter)
+//Se definen las rutas base
+router.use("/auth", authRouter);
+router.use("/api", ApiRouter); // ejemplo:http://localhost:8082/api/popular
+router.use("/showtime", showtimeRouter);
+router.use("/review", reviewRouter);
+router.use("/room", roomRouter);
+router.use("/reservation", reservationRouter);
+router.use("/payment", paymentRouter);
 
-const userModel = require("../models/user.model")
+const userModel = require("../models/user.model");
 
 router.post("/test", async (req, res) => {
-  console.log(await userModel.getById("633cb93893ae09e5243ac4ca"))
-  res.send(await userModel.getById("633cb93893ae09e5243ac4ca"))
-})
+  console.log(await userModel.getById("633cb93893ae09e5243ac4ca"));
+  res.send(await userModel.getById("633cb93893ae09e5243ac4ca"));
+});
 
 //Por si se ingresa una ruta no definida previamente
 router.use("*/*", (req, res, next) => {
-  logger.warn(`${req.protocol + '://' + req.get('host') + req.originalUrl} Not found`)
+  logger.warn(
+    `${req.protocol + "://" + req.get("host") + req.originalUrl} Not found`
+  );
 
   try {
     res.status(404).send(`This page doesn't exists`);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
