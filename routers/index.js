d2c831046d8a245e4ae6c3b55bed9d3d2b2fe0bd
BelenDeAmorrortu <107082075+BelenDeAmorrortu@users.noreply.github.com>
2022-10-06 17:41:18 -0300

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -3,35 +3,24 @@ const logger = require("../utils/logger")
 
 //Se importan todas las rutas
 const ApiRouter = require("./api.router.js")
-<<<<<<< HEAD
-const functionRouter = require("./function.router.js")
 const reviewRouter =  require("./review.router.js")
-const autrhRouter = require("./auth.router.js")
 const paymentRouter = require ("./payment.router.js")
-=======
 const showtimeRouter = require("./showtime.router.js")
-const reviewRouter = require("./review.router.js")
 const roomRouter = require("./room.router")
 const reservationRouter = require("./reservation.router")
 const authRouter = require("./auth.router")
->>>>>>> 7e6af1ce174412dddf0ba06123a5ee7c0a7a640c
 
 const router = express.Router();
 
 //Se definen las rutas base 
 router.use("/auth", authRouter)
 router.use('/api', ApiRouter)// ejemplo:http://localhost:8082/api/popular
-<<<<<<< HEAD
-router.use('/function', functionRouter)
-router.use ('/review', reviewRouter)
-router.use('/auth', autrhRouter)
 router.use('/payment', paymentRouter)
-=======
 router.use('/showtime', showtimeRouter)
 router.use('/review', reviewRouter)
 router.use('/room', roomRouter)
 router.use('/reservation', reservationRouter)
->>>>>>> 7e6af1ce174412dddf0ba06123a5ee7c0a7a640c
+
 
 const userModel = require("../models/user.model")
 
