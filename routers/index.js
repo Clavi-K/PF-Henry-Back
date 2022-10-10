2b009423396fdd3bb6562828ebcccaca52084daf
Angela Testino <angela11021997@gmail.com>
2022-10-10 14:21:02 -0500

    fix

--- routers/index.js
+++ routers/index.js
@@ -7,30 +7,28 @@ const showtimeRouter = require("./showtime.router.js");
 const reviewRouter = require("./review.router.js");
 const roomRouter = require("./room.router");
 const reservationRouter = require("./reservation.router");
-const authRouter = require("./auth.router");
+const userRouter = require("./user.router");
 const paymentRouter = require("./payment.router");
-const listRouter = require("./list.router")
+const listRouter = require("./list.router");
 
 const router = express.Router();
 
 //Se definen las rutas base
-router.use("/auth", authRouter);
+router.use("/user", userRouter);
 router.use("/api", ApiRouter); // ejemplo:http://localhost:8082/api/popular
 router.use("/showtime", showtimeRouter);
 router.use("/review", reviewRouter);
 router.use("/room", roomRouter);
 router.use("/reservation", reservationRouter);
 router.use("/payment", paymentRouter);
-router.use("/list", listRouter)
+router.use("/list", listRouter);
 
-const seatModel = require("../models/seat.model")
+const seatModel = require("../models/seat.model");
 
 router.get("/test", async (req, res, next) => {
-
-  seatModel.hardDelete("6340d0f04bc2dc2a36c4160d")
-  res.send("ok")
-
-})
+  seatModel.hardDelete("6340d0f04bc2dc2a36c4160d");
+  res.send("ok");
+});
 
 //Por si se ingresa una ruta no definida previamente
 router.use("*/*", (req, res, next) => {
